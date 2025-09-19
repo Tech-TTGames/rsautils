ee33b7e17e8c3c009241d20b9135c63e86534abc
Tech. TTGames <50541739+Tech-TTGames@users.noreply.github.com>
2025-09-20 01:31:38 +0300

    Some details about package.

---
+++ src/rsautils/__init__.py
@@ -0,0 +1,27 @@
+"""Various RSA Utilities in an Academic Sense.
+
+Provides RSA Utilities for Encryption, Decryption, Signing, Verification, Exporting Keys to PKCS1 (Public Key) and
+PKCS8 (Private Key). Furthermore, provides various prime-generation utilities under-the-hood.
+"""
+# Copyright (c) 2025-present Tech. TTGames
+# SPDX-License-Identifier: EPL-2.0
+from rsautils.keygen import check_prime
+from rsautils.keygen import export_primes
+from rsautils.keygen import generate_key_pair
+from rsautils.keygen import generate_primes
+from rsautils.keygen import get_pre_primes
+from rsautils.keygen import import_primes
+from rsautils.rsa import RSAPrivKey
+from rsautils.rsa import RSAPubKey
+
+__version__ = "0.0.0"
+__all__ = [
+    "RSAPrivKey",
+    "RSAPubKey",
+    "export_primes",
+    "import_primes",
+    "get_pre_primes",
+    "check_prime",
+    "generate_primes",
+    "generate_key_pair",
+]
