7a90978006a866781028f2ef7c07b2eee7bf1730
Tech. TTGames <50541739+Tech-TTGames@users.noreply.github.com>
2025-10-14 23:20:25 +0300

    Bump version

--- src/rsautils/__init__.py
+++ src/rsautils/__init__.py
@@ -21,7 +21,7 @@ from rsautils.keygen import import_primes
 from rsautils.rsa import RSAPrivKey
 from rsautils.rsa import RSAPubKey
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __all__ = [
     "RSAPrivKey",
     "RSAPubKey",
